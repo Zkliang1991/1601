8a396d2062670879774e3e220ae46dd7b60e8553
fangjin <2119866423@qq.com>
2016-06-28 12:07:54 +0800

    方瑾

---
+++ fangjin/zouxiu/js/footer.js
@@ -0,0 +1,26 @@
+$('.btm-list ul').on('tap','li',function(){
+	$(this).addClass('active').siblings().removeClass('active');
+})
+
+// $('.btm-list ul').on('tap','li',function(){
+// 	var index=$(this).index() ;
+// 	var key=("htmlType_"+index) ;
+// 	alert(typeof key);
+// 	window.location.href=localStorage.key+".html";
+// });
+
+$('.btm-list ul li').eq(0).on('tap',function(){
+	window.location.href=localStorage.htmlType_0+".html";
+})
+$('.btm-list ul li').eq(1).on('tap',function(){
+	window.location.href=localStorage.htmlType_1+".html";
+})
+$('.btm-list ul li').eq(2).on('tap',function(){
+	window.location.href=localStorage.htmlType_2+".html";
+})
+$('.btm-list ul li').eq(3).on('tap',function(){
+	window.location.href=localStorage.htmlType_3+".html";
+})
+$('.btm-list ul li').eq(4).on('tap',function(){
+	window.location.href=localStorage.htmlType_4+".html";
+})
